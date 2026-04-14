fc16ed716ee786671ec4b2e812e7c2c6944476f6
Aditya Kalburgi <adityakalburgi304@gmail.com>
2026-04-14 20:53:41 +0530

    order api endpoint updated

--- src/main/java/com/NexTradeX/order/OrderController.java
+++ src/main/java/com/NexTradeX/order/OrderController.java
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/orders")
+@RequestMapping("/orders")
 @RequiredArgsConstructor
 public class OrderController {
     
