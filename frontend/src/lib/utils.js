e9013f41ebe3ce6be164d0af98afb23b3b114fc0
Aditya Kalburgi <adityakalburgi304@gmail.com>
2026-04-14 20:50:52 +0530

    changed utils functions

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -4,3 +4,30 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs) {
     return twMerge(clsx(inputs));
 }
+
+export function formatCurrency(value, options = {}) {
+    const numericValue = Number(value ?? 0);
+
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: options.currency || "USD",
+        maximumFractionDigits: options.maximumFractionDigits ?? 2,
+        minimumFractionDigits: options.minimumFractionDigits ?? 2,
+        notation: options.notation,
+    }).format(numericValue);
+}
+
+export function formatCompactNumber(value) {
+    const numericValue = Number(value ?? 0);
+
+    return new Intl.NumberFormat("en-US", {
+        notation: "compact",
+        maximumFractionDigits: 2,
+    }).format(numericValue);
+}
+
+export function formatPercent(value) {
+    const numericValue = Number(value ?? 0);
+    const prefix = numericValue > 0 ? "+" : "";
+    return `${prefix}${numericValue.toFixed(2)}%`;
+}
