55ae910e73e9fc1ca606929f2dde79742895ce3c
Aditya Kalburgi <adityakalburgi304@gmail.com>
2026-04-14 20:53:52 +0530

    wallet api endpoint updated

--- src/main/java/com/NexTradeX/wallet/WalletController.java
+++ src/main/java/com/NexTradeX/wallet/WalletController.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/wallets")
+@RequestMapping("/wallets")
 @RequiredArgsConstructor
 public class WalletController {
     
