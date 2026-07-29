42ec21ffefa568942fe37d57abbf9422dc5afada
Aditya Kalburgi <adityakalburgi304@gmail.com>
2026-07-30 00:43:39 +0530

    feat(dca): update DcaSchedule entity and repository methods

--- src/main/java/com/NexTradeX/order/DcaScheduleRepository.java
+++ src/main/java/com/NexTradeX/order/DcaScheduleRepository.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DcaScheduleRepository extends JpaRepository<DcaSchedule, Long> {
     List<DcaSchedule> findAllByUser(User user);
     List<DcaSchedule> findByActiveTrueAndNextRunTimeBefore(LocalDateTime time);
+    Optional<DcaSchedule> findByIdAndUser(Long scheduleId, User user);
 }
