36bd0d056906a89f0a448327510667b287ba2cb8
Aditya Kalburgi <adityakalburgi304@gmail.com>
2026-08-03 00:31:51 +0530

    refactor(core): migrate IRiskManagementService to Enterprise LLD package layout

---
+++ backend/src/main/java/com/NexTradeX/modules/risk/IRiskManagementService.java
@@ -0,0 +1,8 @@
+package com.nextradex.modules.risk;
+
+/**
+ * Interface contract for Risk Management operations.
+ */
+public interface IRiskManagementService {
+    void monitorAndLiquidatePositions();
+}
