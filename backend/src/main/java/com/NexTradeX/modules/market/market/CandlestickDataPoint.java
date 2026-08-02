c76a1d3a5b65d762efe63ea1677c4350a678e9dd
Aditya Kalburgi <adityakalburgi304@gmail.com>
2026-08-03 00:31:48 +0530

    refactor(core): migrate CandlestickDataPoint to Enterprise LLD package layout

---
+++ backend/src/main/java/com/NexTradeX/modules/market/market/CandlestickDataPoint.java
@@ -0,0 +1,22 @@
+package com.nextradex.modules.market.market;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CandlestickDataPoint {
+
+    private long time;
+    private BigDecimal open;
+    private BigDecimal high;
+    private BigDecimal low;
+    private BigDecimal close;
+    private BigDecimal volume;
+}
