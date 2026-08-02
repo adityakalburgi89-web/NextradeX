7dacb1d51c971ae2ad9d67896d82303735bbe140
Aditya Kalburgi <adityakalburgi304@gmail.com>
2026-08-03 00:32:04 +0530

    refactor(core): migrate OrderType to Enterprise LLD package layout

---
+++ backend/src/main/java/com/NexTradeX/modules/trading/order/OrderType.java
@@ -0,0 +1,10 @@
+package com.nextradex.modules.trading.order;
+
+public enum OrderType {
+    MARKET,
+    LIMIT,
+    STOP_MARKET,
+    STOP_LIMIT,
+    TAKE_PROFIT_MARKET,
+    TAKE_PROFIT_LIMIT
+}
